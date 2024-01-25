fbca8aa59d3ac6b3520b5d0a68d1b9370a2693b6
mateusSleite <mateusleite2709@gmail.com>
2024-01-24 22:27:44 -0300

    MotoBotAtt

--- obj/Debug/net7.0-windows/jogo.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/jogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb1ac0476096416613dcf30a9cbb01fbb489189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("jogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
