8b08868358f45c6dfda316f09e34fea5cdfe168c
Renato Mendes <rnt.mendes11@gmail.com>
2024-01-28 14:56:41 -0300

    Layout Buttons Menu

--- Menu.cs
+++ Menu.cs
@@ -14,12 +14,11 @@ public class Menu : Form
 
         this.WindowState = FormWindowState.Maximized;
         this.Text = "Dangerous Delivery";
-        this.BackgroundImage = Image.FromFile("./Image/Menu (2).jpg");
+        this.BackgroundImage = Image.FromFile("./Image/Menu/Menu.jpg");
 
-
-        var playButton = CriarBotao("Jogar");
-        var opcoesButton = CriarBotao("Opções");
-        var exitButton = CriarBotao("Sair");
+        var playButton = CriarBotao("./Image/Menu/jogarButton.png");
+        var opcoesButton = CriarBotao("./Image/Menu/opcoesButton.png");
+        var exitButton = CriarBotao("./Image/Menu/sairButton.png");
 
         playButton.Click += (o, e) =>
         {
@@ -27,11 +26,13 @@ public class Menu : Form
             this.Hide();
             game.Show();
         };
+
         opcoesButton.Click += (o, e) =>
         {
             Opcoes opcoesForm = new Opcoes();
             opcoesForm.Mostrar();
         };
+
         exitButton.Click += (o, e) =>
         {
             Application.Exit();
@@ -46,9 +47,9 @@ public class Menu : Form
             pb.Image = bmp;
             timer.Start();
 
-            CentralizarBotao(playButton);
-            CentralizarBotao(opcoesButton);
-            CentralizarBotao(exitButton);
+            CentralizarBotao(playButton, -250);
+            CentralizarBotao(opcoesButton, 0);
+            CentralizarBotao(exitButton, 250);
         };
 
         timer.Tick += (o, e) =>
@@ -57,19 +58,33 @@ public class Menu : Form
         };
     }
 
-    private Button CriarBotao(string texto)
+    private Button CriarBotao(string imagemPath)
     {
         var botao = new Button();
-        botao.Text = texto;
-        botao.Size = new Size(100, 50);
+        botao.Size = new Size(400, 150);
+
+        var imagem = Image.FromFile(imagemPath);
+
+        botao.Image = new Bitmap(imagem, botao.Size);
+        botao.ImageAlign = ContentAlignment.MiddleLeft;
+        botao.BackColor = Color.Transparent;
+
+        botao.TextImageRelation = TextImageRelation.ImageBeforeText;
+        botao.ImageAlign = ContentAlignment.MiddleCenter;
+
+        botao.FlatStyle = FlatStyle.Flat;
+        botao.FlatAppearance.BorderSize = 0;
+        botao.FlatAppearance.MouseOverBackColor = Color.Transparent;
+        botao.FlatAppearance.MouseDownBackColor = Color.Transparent;
+
         this.Controls.Add(botao);
         return botao;
     }
 
-    private void CentralizarBotao(Button botao)
+    private void CentralizarBotao(Button botao, int Position)
     {
         botao.Location = new Point(
-            (this.ClientSize.Width - botao.Width) / 2, 
-            (this.ClientSize.Height - botao.Height) / 2 + botao.TabIndex * 100);
+            (this.ClientSize.Width - botao.Width) / 2,
+            (this.ClientSize.Height - botao.Height) / 2 + Position);
     }
 }
