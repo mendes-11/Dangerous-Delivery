c3f18b9db6a65c0e492c173e84b76bf485f3b495
Renato Mendes <rnt.mendes11@gmail.com>
2024-01-28 17:28:35 -0300

    BackGround centralized

--- Menu.cs
+++ Menu.cs
@@ -4,21 +4,23 @@ using System.Windows.Forms;
 
 public class Menu : Form
 {
-    private Bitmap bmp;
-    private Graphics g;
+    private PictureBox pbBackground;
+    private Button playButton, opcoesButton, exitButton;
 
     public Menu()
     {
-        var pb = new PictureBox { Dock = DockStyle.Fill, BackColor = Color.Transparent };
-        var timer = new Timer { Interval = 20 };
+        WindowState = FormWindowState.Maximized;
 
-        this.WindowState = FormWindowState.Maximized;
-        this.Text = "Dangerous Delivery";
-        this.BackgroundImage = Image.FromFile("./Image/Menu/Menu.jpg");
+        pbBackground = new PictureBox
+        {
+            Dock = DockStyle.Fill,
+            Image = Image.FromFile("./Image/Menu/Menu.jpg"),
+            SizeMode = PictureBoxSizeMode.StretchImage
+        };
 
-        var playButton = CriarBotao("./Image/Menu/jogarButton.png");
-        var opcoesButton = CriarBotao("./Image/Menu/opcoesButton.png");
-        var exitButton = CriarBotao("./Image/Menu/sairButton.png");
+        playButton = CriarBotao("./Image/Menu/jogarButton.png");
+        opcoesButton = CriarBotao("./Image/Menu/opcoesButton.png");
+        exitButton = CriarBotao("./Image/Menu/sairButton.png");
 
         playButton.Click += (o, e) =>
         {
@@ -38,53 +40,44 @@ public class Menu : Form
             Application.Exit();
         };
 
-        this.Controls.Add(pb);
+        this.Controls.Add(pbBackground);
+
+        pbBackground.Controls.Add(playButton);
+        pbBackground.Controls.Add(opcoesButton);
+        pbBackground.Controls.Add(exitButton);
 
         this.Load += (o, e) =>
         {
-            bmp = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
-            g = Graphics.FromImage(bmp);
-            pb.Image = bmp;
-            timer.Start();
-
             CentralizarBotao(playButton, -250);
             CentralizarBotao(opcoesButton, 0);
             CentralizarBotao(exitButton, 250);
-        };
-
-        timer.Tick += (o, e) =>
-        {
-            pb.Refresh();
         };
     }
 
     private Button CriarBotao(string imagemPath)
     {
-        var botao = new Button();
-        botao.Size = new Size(400, 150);
+        var botao = new Button
+        {
+            Size = new Size(400, 150),
+            Image = new Bitmap(Image.FromFile(imagemPath), new Size(390, 150)),
+            ImageAlign = ContentAlignment.MiddleLeft,
+            BackColor = Color.Transparent,
+            FlatStyle = FlatStyle.Flat,
+            FlatAppearance = { BorderSize = 0 },
+            TextAlign = ContentAlignment.MiddleCenter
+        };
 
-        var imagem = Image.FromFile(imagemPath);
-
-        botao.Image = new Bitmap(imagem, botao.Size);
-        botao.ImageAlign = ContentAlignment.MiddleLeft;
-        botao.BackColor = Color.Transparent;
-
-        botao.TextImageRelation = TextImageRelation.ImageBeforeText;
-        botao.ImageAlign = ContentAlignment.MiddleCenter;
-
-        botao.FlatStyle = FlatStyle.Flat;
-        botao.FlatAppearance.BorderSize = 0;
         botao.FlatAppearance.MouseOverBackColor = Color.Transparent;
         botao.FlatAppearance.MouseDownBackColor = Color.Transparent;
 
-        this.Controls.Add(botao);
         return botao;
     }
 
-    private void CentralizarBotao(Button botao, int Position)
+    private void CentralizarBotao(Button botao, int position)
     {
         botao.Location = new Point(
-            (this.ClientSize.Width - botao.Width) / 2,
-            (this.ClientSize.Height - botao.Height) / 2 + Position);
+            (pbBackground.ClientSize.Width - botao.Width) / 2,
+            (pbBackground.ClientSize.Height - botao.Height) / 2 + position
+        );
     }
 }
--- obj/Debug/net7.0-windows/jogo.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/jogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0897ee6f6c1f93deb5ab592769a98c2bdbe67d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b08868358f45c6dfda316f09e34fea5cdfe168c")]
 [assembly: System.Reflection.AssemblyProductAttribute("jogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
