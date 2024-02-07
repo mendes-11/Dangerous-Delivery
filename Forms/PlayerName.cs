6dd289c00f6e4a50629d4b6adb31d2ce9859d553
mateusSleite <mateusleite2709@gmail.com>
2024-02-07 16:49:06 -0300

    PlayerNameIndo

--- Forms/PlayerName.cs
+++ Forms/PlayerName.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 public class PlayerNameForm : Form
 {
     private Label nameLabel;
     private TextBox nameTextBox;
+    private PictureBox pbBackground;
     private Button okButton;
 
     public string PlayerName { get; private set; }
@@ -16,27 +18,41 @@ public class PlayerNameForm : Form
 
     private void InitializeComponent()
     {
-        nameLabel = new Label();
-        nameTextBox = new TextBox();
-        okButton = new Button();
+        // Configurar o PictureBox para ser o plano de fundo
+        pbBackground = new PictureBox
+        {
+            Dock = DockStyle.Fill,
+            BackColor = Color.Transparent,
+            Image = Image.FromFile("./Image/Menu/name.png"),
+            SizeMode = PictureBoxSizeMode.StretchImage
+        };
+        Controls.Add(pbBackground);
 
-        nameLabel.Text = "Digite seu nome:";
-        nameLabel.Location = new System.Drawing.Point(10, 10);
+        // Configurar os controles de entrada
+        // nameLabel = new Label();
+        // nameLabel.Text = "Digite seu nome:";
+        // nameLabel.Location = new Point(10, 10);
+        // pbBackground.Controls.Add(nameLabel);
 
-        nameTextBox.Location = new System.Drawing.Point(10, 30);
-        nameTextBox.Size = new System.Drawing.Size(200, 20);
+        // nameTextBox = new TextBox();
+        // nameTextBox.Location = new Point(10, 30);
+        // nameTextBox.Size = new Size(200, 20);
+        // pbBackground.Controls.Add(nameTextBox);
 
-        okButton.Text = "OK";
-        okButton.Location = new System.Drawing.Point(10, 60);
-        okButton.Click += OkButtonClick;
+        // okButton = new Button();
+        // okButton.Text = "OK";
+        // okButton.Location = new Point(10, 60);
+        // okButton.Click += OkButtonClick;
+        // pbBackground.Controls.Add(okButton);
 
-        Controls.Add(nameLabel);
-        Controls.Add(nameTextBox);
-        Controls.Add(okButton);
-
-        Size = new System.Drawing.Size(600, 300);
+        // Configurações do formulário
+        Size = new Size(900, 450);
         FormBorderStyle = FormBorderStyle.None;
         StartPosition = FormStartPosition.CenterScreen;
+
+        // Definir cor de fundo transparente
+        this.TransparencyKey = Color.FromArgb(0, 0, 1);
+        this.BackColor = Color.FromArgb(0, 0, 1);
     }
 
     private void OkButtonClick(object sender, EventArgs e)
